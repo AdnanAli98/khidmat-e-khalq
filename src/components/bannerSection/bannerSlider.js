939f3b54f582b92ed72e7477a419947f4d56b6db
Adnan Ali <adnanshah7272.as@gmail.com>
2025-03-27 13:40:57 +0500

    banner slider changed to swiper js

--- src/components/bannerSection/bannerSlider.js
+++ src/components/bannerSection/bannerSlider.js
@@ -1,48 +1,66 @@
 "use client";
 
-import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import styles from "./slider.module.scss";
 import { sliderData } from "./sliderData";
+import { useState } from "react";
 
 const BannerSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  const handleSlideChange = (swiper) => {
+    setActiveIndex(swiper.activeIndex);
+  }
   return (
     <div className={styles.sliderContainer}>
-      {sliderData.map((slide, index) => (
-        <div
-          key={index}
-          className={`${styles.slide} ${index === currentIndex ? styles.active : ""}`}
-          style={{ backgroundImage: `url(${slide.image})` }}
-        >
-          <div className={styles.overlay}></div>
-
-          <div className={styles.contentWrapper}>
-            <div className={styles.content}>
-              <h2 className={styles.title}>{slide.title}</h2>
-              <p className={styles.description}>{slide.description}</p>
-              <div className={styles.buttons}>
-                <button className={`${styles.button} ${styles.register}`}>
-                Register as Volunteer
-                </button>
-                <button className={`${styles.button} ${styles.involved}`}>
-                  Get Involved
-                </button>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        onSlideChange={(swiper) => handleSlideChange(swiper)}
+        pagination={{
+          clickable: true,
+          renderBullet: (index, className) => {
+            return `<span class="${className} ${index === activeIndex ? `${styles.swiperPaginationBulletActive}` : `${styles.swiperPaginationBullet}`}">
+                    </span>`;
+          }
+        }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        slidesPerView={1}
+        className={styles.swiperContainer}
+      >
+        {sliderData.map((slide, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <div className={styles.slideWrapper}>
+              <div
+                className={styles.slide}
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                }}
+              >
+                <div className={styles.overlay}></div>
+                <div className={styles.contentWrapper}>
+                  <div className={styles.content}>
+                    <h2 className={styles.title}>{slide.title}</h2>
+                    <p className={styles.description}>{slide.description}</p>
+                    <div className={styles.buttons}>
+                      <button className={`${styles.button} ${styles.register}`}>
+                        Register as Volunteer
+                      </button>
+                      <button className={`${styles.button} ${styles.involved}`}>
+                        Get Involved
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      ))}
-
-      <div className={styles.dots}>
-        {sliderData.map((_, index) => (
-          <button
-            key={index}
-            className={`${styles.dot} ${index === currentIndex ? styles.activeDot : ""}`}
-            onClick={() => setCurrentIndex(index)}
-          />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
+
+      <div className={`${styles.swiperPagination} customPagination`}></div>
     </div>
   );
 };
